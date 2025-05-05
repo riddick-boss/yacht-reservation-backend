1179222559895448ce313f42b03b74a834a48456
Pawel Kremienowski <kremienowski33@gmail.com>
2025-05-05 23:29:22 +0200

    Initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,8 @@ dependencies {
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
     implementation(libs.ktor.server.config.yaml)
+    implementation(libs.koin)
+    implementation(libs.koin.logger)
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
 }
